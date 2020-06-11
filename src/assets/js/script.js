46fec6770141786577b5a2b04cc1eefa93c88377
panduwijayasaputra <panduwijayasaputra@gmail.com>
2020-06-11 17:58:56 +0700

    commit

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,46 @@
 // Script Here
 window.$ = window.jQuery = require("jquery");
 
-$(document).ready(function () {});
+let slideIndex = 1;
+
+showSlide(slideIndex);
+
+function showSlide(n) {
+  var slides = $("#heroSlider").children(".slider__item");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.addClass("d-none");
+  $("#heroSlider")
+    .children(".slider__item")
+    .eq(slideIndex - 1)
+    .removeClass("d-none");
+
+  setTimeout(() => {
+    slideIndex++;
+    showSlide(slideIndex);
+  }, 10000);
+}
+
+function backSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+$(document).ready(function () {
+  $("#slider1_prev").click(function () {
+    backSlide();
+  });
+  $("#slider1_next").click(function () {
+    nextSlide();
+  });
+});
