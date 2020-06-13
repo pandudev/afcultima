ebfc9e05b3f3a48d1c9952c23ad0d19752480ed8
panduwijayasaputra <panduwijayasaputra@gmail.com>
2020-06-14 00:35:27 +0700

    doctor ok

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,49 +1,6 @@
 // Script Here
 window.$ = window.jQuery = require("jquery");
 
-// let slideIndex = 1;
-// let doctorIndex = 0;
-
-// showSlide(slideIndex);
-// doctorSlide();
-
-// function showSlide(n) {
-//   var slides = $("#heroSlider").children(".slider__item");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-
-//   slides.addClass("d-none");
-//   $("#heroSlider")
-//     .children(".slider__item")
-//     .eq(slideIndex - 1)
-//     .removeClass("d-none");
-// }
-
-// function doctorSlide() {
-//   var doctors = $(".doctor__testimonial__say");
-//   doctors.addClass("d-none");
-//   if (doctorIndex >= doctors.length) {
-//     doctorIndex = 0;
-//   }
-
-//   console.log(doctorIndex);
-//   doctors.eq(doctorIndex).removeClass("d-none");
-// }
-
-function backSlide() {
-  slideIndex--;
-  showSlide(slideIndex);
-}
-
-function nextSlide() {
-  slideIndex++;
-  showSlide(slideIndex);
-}
-
 function heroTransition() {
   $(".slider__item h1").addClass("fade");
   $(".slider__item h2").addClass("fade");
@@ -56,14 +13,29 @@ function heroRemoveTransition() {
   $(".slider__item a").removeClass("fade");
 }
 
+function showDoctor(n) {
+  console.log($(".doctors").children());
+  $(".doctors").children().removeClass("show");
+  $(".doctors").children().eq(n).addClass("show");
+}
+
 $(document).ready(function () {
+  showDoctor(0);
   heroTransition();
   $("#hamburger").click(function () {
     $(this).toggleClass("active");
     $(".mobile-menu").toggleClass("show");
     $("body").toggleClass("fixed");
   });
-
+  $(".doctor__testimonial__slider").on("afterChange", function (
+    event,
+    slick,
+    currentSlide,
+    nextSlide
+  ) {
+    console.log(currentSlide);
+    showDoctor(currentSlide);
+  });
   $(".doctor__testimonial__slider").slick({
     autoplay: true,
     arrows: false,
@@ -73,21 +45,11 @@ $(document).ready(function () {
     arrows: false,
   });
 
-  $(".hero__slider").on("beforeChange", function (
-    event,
-    slick,
-    currentSlide,
-    nextSlide
-  ) {
+  $(".hero__slider").on("beforeChange", function () {
     heroRemoveTransition();
   });
 
-  $(".hero__slider").on("afterChange", function (
-    event,
-    slick,
-    currentSlide,
-    nextSlide
-  ) {
+  $(".hero__slider").on("afterChange", function () {
     heroTransition();
   });
   $(".hero__slider").slick({
